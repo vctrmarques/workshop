1ae7761611da5552d5369e9f9481896937d819e7
joao.marques <vctmarques@gmail.com>
2020-06-03 23:25:06 -0300

    first commit

--- db.js
+++ db.js
@@ -1,10 +1,19 @@
 var mongoClient = require('mongodb').MongoClient;
-mongoClient.connect('vctmarques:pintado05@kamino.mongodb.umbler.com:44252/workshp')
- .then(conn => global.conn = conn)
- .catch(err => console.log(err))
+mongoClient.connect('vctmarques:pintado05@kamino.mongodb.umbler.com:44252/workshop')
+   .then(conn => global.conn = conn)
+   .catch(err => console.log(err))
 
- function findAll(callback){
-    global.conn.collection('customers').find({}).toArray(callback);
-   }
+function findAll(callback) {
+   global.conn.collection('customers').find({}).toArray(callback);
+}
 
-module.exports = { findAll }
+function insert(customer, callback) {
+   global.conn.collection('customers').insert(customer, callback);
+}
+
+function deleteOne(id, callback) {
+   global.conn.collection('customers').deleteOne({ _id: new ObjectId(id) },
+      callback);
+}
+
+module.exports = { findAll, insert, deleteOne }
--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,29 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   global.db.findAll((e, docs) => {
-    if(e) { return console.log(e); }
+    if (e) { return console.log(e); }
     res.render('index', { docs });
-    })
+  })
+});
+
+router.post('/new', function (req, res, next) {
+  const nome = req.body.nome;
+  const idade = parseInt(req.body.idade);
+  const uf = req.body.uf;
+  global.db.insert({ nome, idade, uf }, (err, result) => {
+    if (err) { return console.log(err); }
+    res.redirect('/');
+  })
+});
+
+router.get('/delete/:id', function (req, res) {
+  var id = req.params.id;
+  global.db.deleteOne(id, (e, r) => {
+    if (e) { return console.log(e); }
+    res.redirect('/');
+  });
 });
 
 module.exports = router;
